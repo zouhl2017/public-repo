fe012252802fe53c4dff4854f77f29caa883173f
zouhl2017 <zouhl1994@163.com>
2018-09-16 17:06:44 +0800

    create a java demo

--- aop/src/main/java/com/zafkiel/Service/impl/MyServiceImpl.java
+++ aop/src/main/java/com/zafkiel/Service/impl/MyServiceImpl.java
@@ -6,6 +6,32 @@ import org.springframework.stereotype.Service;
 @Service
 public class MyServiceImpl implements IMyService {
 
+    private static int[] execute3(int[] array, int m) {
+        int[] returnArr=new int[2];
+        int first = 0;
+        int last = array.length -1;
+        int sum = 0;
+        while(first < last ) {
+            sum = array[first] + array[last];
+            if (sum == m) {
+                returnArr[0]=first+1;
+                returnArr[1]=last+1;
+                break;
+            } else if (sum < m) {
+                first++;
+            } else {
+                last--;
+            }
+        }
+        return returnArr;
+    }
+
+    public  static void  main(String[] args){
+        int[] array=new int[]{1,2,3,4,5,6,7,8,9,10};
+      int[] arr=  execute3(array,3);
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+    }
     public final   void  notBanana(){
         System.out.println("not banana");
     }
